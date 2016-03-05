7ae803659ea8c76b4ab89850665b8213564c64c9
george <hulingfeng211@163.com>
2016-03-06 00:14:46 +0800

    功能优化

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), 'templates')
 # TEMPLATE_PATH=os.path.join(os.path.dirname(__file__),'front')
 
 # REDIS Server相关配置
-REDIS_HOST = '192.168.2.14'
+REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 MAX_CONNECTIONS = 1024
 REDIS_DB = 5
@@ -56,8 +56,8 @@ LOGIN_URL = "/page/login"
 XSRF_COOKIES = True
 
 # MONGODB设置
-MONGO_URI = "mongodb://192.168.2.14:27017/"
-DB_NAME = 'mywork'
+MONGO_URI = "mongodb://localhost:27017/"
+DB_NAME = 'test'
 
 # 站点设置
 SITE_NAME = "管理系统模板Demo"
